f9ccdec0c685db4d64687b3507692861130c2cce
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-02-05 16:24:13 -0300

    Created my first component\o/

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,26 @@
 <template>
-  <div class="flex w-screen h-screen text-gray-700">
-    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
-      <!-- sidebar -->
-    </div>
-
-    <div class="flex flex-col flex-grow">
-      <!-- Content -->
-      <div class="flex flex-col flex-grow overflow-auto">
-          <editor-content :editor="editor" />
+    <div class="flex w-screen h-screen text-gray-700">
+      <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+        <!-- sidebar -->
       </div>
-    </div>
+
+      <div class="flex flex-col flex-grow">
+        <!-- Content -->
+        <div class="flex flex-col flex-grow overflow-auto">
+          <tip-tap />
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import { Editor, EditorContent } from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
+import TipTap from "./components/TipTap.vue"
 
 export default {
   name: 'App',
 
   components: {
-    EditorContent,
-  },
-
-  data() {
-    return {
-      editor: null,
-    }
-  },
-
-  mounted() {
-    this.editor = new Editor({
-      content: '<h3>Type something...</h3>',
-      extensions: [
-        StarterKit,
-      ],
-      editorProps: {
-        attributes: {
-          class: "prose my-6 mx-auto focus:outline-none"
-        }
-      }
-    })
-  },
-
-  beforeUnmount() {
-    this.editor.destroy()
-  },
+    TipTap
+  }
 }
 </script>
---
+++ src/components/TipTap.vue
@@ -0,0 +1,40 @@
+<template>
+    <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+    name: 'TipTap',
+
+    components: {
+        EditorContent
+    },
+
+    data() {
+        return {
+            editor: null,
+        }
+    },
+
+    mounted() {
+        this.editor = new Editor({
+        content: '<h3>Type something...</h3>',
+        extensions: [
+            StarterKit,
+        ],
+        editorProps: {
+            attributes: {
+            class: "prose my-6 mx-auto focus:outline-none"
+            }
+        }
+        })
+    },
+
+    beforeUnmount() {
+        this.editor.destroy()
+    },
+}
+</script>
