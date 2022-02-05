3f3fff9ee3a0e2c72f1692ae2d33af1816ef3e4f
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-02-05 16:53:29 -0300

    Now I can save a note :)

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,75 @@
     <div class="flex w-screen h-screen text-gray-700">
       <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
         <!-- sidebar -->
+        <ul v-for="(note, index) in allNotes" :key="index">
+          <li>
+            <p>{{ note.note }}</p>
+          </li>
+        </ul>
+
       </div>
 
       <div class="flex flex-col flex-grow">
         <!-- Content -->
-        <div class="flex flex-col flex-grow overflow-auto">
-          <tip-tap />
+        <div class="flex flex-col flex-grow overflow-auto m-2">
+          <label class="mb-2 text-gray-700" for="note">Type some note...</label>
+          <textarea 
+            v-model="note"
+            id="note"
+            class="border border-gray-300 rounded focus:outline-none"
+            />
+
+            <button class="bg-gray-400 rounded p-2 mt-2" v-on:click="saveNote">Salvar Nota</button>
         </div>
       </div>
   </div>
 </template>
 
 <script>
-import TipTap from "./components/TipTap.vue"
-
 export default {
   name: 'App',
 
   components: {
-    TipTap
+    //
+  },
+
+  data: function () {
+    return {
+      note: '',
+      notes: []
+    }
+  },
+
+  methods: {
+    saveNote() {
+      if(this.note.trim() != '') {
+
+        if(confirm('Are you sure you want to save this note?')) {
+          this.notes.push({
+            note: this.note
+          });
+
+          this.note = ''
+        }
+      }
+    }
+  },
+
+  computed: {
+    allNotes() {
+      let notes = this.notes;
+
+      // check empty array
+      if(notes.length === 0) {
+        return [{
+          note: 'Add some note your son of a bitch'
+        }];
+      }
+
+      return notes;
+    }
   }
+
+
 }
 </script>
