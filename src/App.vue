df23ff1d9280d722a9f0558750fef12d9dd75764
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-02-05 07:01:19 -0300

    Added tiptap

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,42 @@
     </div>
 
     <div class="flex flex-col flex-grow">
-      
+      <!-- Content -->
+      <div class="flex flex-col flex-grow overflow-auto">
+          <editor-content :editor="editor" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
 export default {
   name: 'App',
+
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+  },
+
+  beforeUnmount() {
+    this.editor.destroy()
+  },
 }
 </script>
